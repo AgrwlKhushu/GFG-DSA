2e599c5268b6491de81daa8d9f3dedb72e6ede3a
Khushi Agarwal <agrwlkhushi27@gmail.com>
2024-12-01 22:11:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String st = sc.next();
+
+            char ans = new Solution().nonRepeatingChar(st);
+
+            if (ans != '$')
+                System.out.println(ans);
+            else
+                System.out.println(-1);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static char nonRepeatingChar(String s) {
+        // Your code here
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        
+        for(Map.Entry<Character, Integer> e: map.entrySet()){
+            if(e.getValue()<2){
+                return e.getKey();
+            }
+        }
+        return '$';
+        
+    }
+}
