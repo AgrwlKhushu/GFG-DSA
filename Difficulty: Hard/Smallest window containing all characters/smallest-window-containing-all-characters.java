d87720aca3815359590a4e43f4b8531cb6407d6f
Khushi Agarwal <agrwlkhushi27@gmail.com>
2026-03-06 23:23:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,23 +1,57 @@
 class Solution {
-    public static String smallestWindow(String s, String p) {
-        if (s.length() < p.length()) return "";
-        int[] need = new int[128];
-        for (char c : p.toCharArray()) need[c]++;
-        int required = p.length();
-        int left = 0, minLen = Integer.MAX_VALUE, start = 0;
-        for (int right = 0; right < s.length(); right++) {
-            if (need[s.charAt(right)] > 0) required--;
-            need[s.charAt(right)]--;
-            while (required == 0) {
+    public static String minWindow(String s, String p) {
+        // code here
+        
+        int len1 = s.length();
+        int len2 = p.length();
+        
+        if (len1 < len2) return "";
+        
+        int[] countP = new int[26];
+        int[] countW = new int[26];
+        
+        for (int i = 0; i < len2; i++) {
+            countP[p.charAt(i) - 'a']++;
+        }
+        
+        int required = len2;
+        int formed = 0;
+        
+        int left = 0;
+        int right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int minStart = 0;
+        
+        while (right < len1) {
+            char c = s.charAt(right);
+            int idx = c - 'a';
+            
+            countW[idx]++;
+            
+            if (countP[idx] > 0 && countW[idx] <= countP[idx]) {
+                formed++;
+            }
+            
+            while (formed == required && left <= right) {
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
-                    start = left;
+                    minStart = left;
                 }
-                need[s.charAt(left)]++;
-                if (need[s.charAt(left)] > 0) required++;
+                
+                char d = s.charAt(left);
+                int idy = d - 'a';
+                countW[idy]--;
+                
+                if (countP[idy] > 0 && countW[idy] < countP[idy]) {
+                    formed--;
+                }
+                
                 left++;
             }
+            
+            right++;
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
